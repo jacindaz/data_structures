3a5d0f583367d0bf7852e2d3d72d1b35d3d1d3cb
Jacinda Zhong <jacindaz@gmail.com>
2018-04-03 13:31:35 -0700

    Wk1,p2: split up building tree and tree traversal into different methods

--- week1_problems/tree_height/tree-height.py
+++ week1_problems/tree_height/tree-height.py
@@ -9,49 +9,34 @@ class TreeHeight:
         self.n = int(sys.stdin.readline())
         self.parent = list(map(int, sys.stdin.readline().split()))
 
-    def compute_height(self):
-        # Suggestion: Take advantage of the fact that the labels
-        # for each tree node are integers in the range 0..n-1:
-        #  => you can store each node in an array whose index is the label of the node.
-        #  => By storing the nodes in an array, you have O(1)
-        #     access to any node given its label.
-
-        # Create an array of n nodes:
-        # allocate nodes[n]
-        # for i=0 to n-1:
-        #   nodes[i] = new Node
-        # isn't self.parent already what this psuedocode is asking for???
+    def build_tree(self):
         print(f'self.parent: {self.parent}')
 
         nodes = []
-        # first add all the parents to the nodes
         for parent in self.parent:
             nodes.append({ 'parent': parent, 'children': [] })
         print(f'\nnodes: {nodes}\n')
 
-        # then read each parent index:
-        # for child_index=0 to n-1:
-        #   read parent_index
-        #   if parent_index == -1:
-        #       root = child_index
-        #   else:
-        #       nodes[parent_index].addChild(nodes[child_index])
-
-        # add children to nodes array
         for child_index, parent_index in enumerate(self.parent):
             print('-------------------')
             print(f'child_index: {child_index}, parent_index: {parent_index}')
             if parent_index == -1:
                 root = child_index
             else:
-                # first element: 0
-                # parent_index is 4
-                # so find parent node, which is at index 4
-                # and add child to that node
                 nodes[parent_index]['children'].append(child_index)
                 print('adding child to parent node')
                 print(f'\nnodes: {nodes}\n')
 
+    def compute_height(self):
+        # Once you’ve built the tree, you’ll then need to compute its height.
+        # If you don’t use recursion, you needn’t worry about stack overflow problems.
+        # Without recursion, you’ll need some auxiliary data structure to keep
+        # track of the current state (in the breadth-first seach code in lecture,
+        # for example, we used a queue).
+
+        # use breadth-first search
+        # and a queue
+        # to traverse the tree
 
 
 
