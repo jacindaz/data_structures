bf5f7ea99a0569ec26e29fe263bb384c221f6a5e
Jacinda Zhong <jacindaz@gmail.com>
2018-04-03 19:03:09 -0700

    Wk1,p2: WIP for tree height

--- week1_problems/tree_height/tree-height.py
+++ week1_problems/tree_height/tree-height.py
@@ -13,20 +13,20 @@ class TreeHeight:
         self.tree = self.build_tree()
 
     def build_tree(self):
-        print(f'self.parent: {self.parent}')
+        # print(f'self.parent: {self.parent}')
 
         nodes = {}
         for parent in self.parent:
             nodes[parent] = []
-        print(f'\nnodes: {nodes}\n')
+        # print(f'\nnodes: {nodes}\n')
 
         for child_index, parent_index in enumerate(self.parent):
-            print('-------------------')
-            print(f'child_index: {child_index}, parent_index: {parent_index}')
+            # print('-------------------')
+            # print(f'child_index: {child_index}, parent_index: {parent_index}')
             nodes[parent_index].append(child_index)
 
-            print('adding child to parent node')
-            print(f'\nnodes: {nodes}\n')
+            # print('adding child to parent node')
+            # print(f'\nnodes: {nodes}\n')
         return nodes
 
     def compute_height(self):
@@ -36,53 +36,48 @@ class TreeHeight:
         # track of the current state (in the breadth-first seach code in lecture,
         # for example, we used a queue).
 
-        # use breadth-first search
-        # and a queue
-        # to traverse the tree
-        #
-        # Code from Lecture:
-        # if tree = nil:
-        #     return
-        # queue = []
-        # queue.enqueue(tree)
-        # while not queue.empty():
-        #     node = queue.dequeue()
-        #     print(node)
-        #     if node.left != nil:
-        #         queue.enqueue(node.left)
-        #     if node.right != nil:
-        #         queue.enqueue(node.right)
+        # sorted tree
+        # {-1: [9], 2: [4, 8], 5: [2, 3], 7: [1], 9: [0, 5, 6, 7]}
 
-        # self.tree
-        # [
-        #     {'parent': 4, 'children': []},
-        #     {'parent': -1, 'children': [3, 4]},
-        #     {'parent': 4, 'children': []},
-        #     {'parent': 1, 'children': []},
-        #     {'parent': 1, 'children': [0, 2]}
-        # ]
+        sorted_tree = dict(sorted(self.tree.items()))
+        queue = deque(sorted_tree)
 
-        print(f'self.tree: {self.tree}')
-        if len(self.tree) == 0:
-            return 0
-        else:
-            queue = deque(self.tree)
-            while len(queue) > 0:
-                # get first node in tree
-                node = queue.popleft()
-                print(f'\nnode: {node}')
-                print(f'queue: {queue}')
+        max_height = 0
+        current_level = 1
+        while len(queue) > 0:
+            node = queue.popleft() # -1
+            root_node = sorted_tree[node][0] # [9]
+            root_node_children = sorted_tree[root_node]
+
+            # [0, 5, 6, 7]
+            children_queue = deque(root_node_children)
+            while len(children_queue) > 0:
+                # for node in root_node_children:
+                node = children_queue.popleft()
+                print('-----------------------------')
+                print(f'root_node_children: {root_node_children}, children_queue: {children_queue}')
+                print(f'current node: {node}, current_level: {current_level}')
+                current_level += 1
+
+                # start with 0
+                # does 0 have any children?
+
+                # if has children
+                if node in sorted_tree:
+                    children = sorted_tree[node]
+                    print(f'\nnode ({node}) found, has children.')
+                    print(f'children: {children}')
+
+                    current_level += 1
 
 
-# Also, if you are performing many pop(0), you should look at collections.deque
-
-# from collections import deque
-# >>> l = deque(['a', 'b', 'c', 'd'])
-# >>> l.popleft()
-# 'a'
-# >>> l
-# deque(['b', 'c', 'd'])
-# Provides higher performance popping from left end of the list
+                # if does not have children
+                else:
+                    print(f'\nnode ({node}) does not have any children.')
+                    print('moving one level back up. ')
+                    if current_level > max_height:
+                        max_height = current_level
+                    current_level -= 1
 
 
 
