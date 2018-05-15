e0c0906c3152b8cf4d0df372423ec40a6e4f2e5a
Jacinda Zhong <jacindaz@gmail.com>
2018-05-15 11:30:47 -0700

    Linked list, insert at head method

--- other_practice/linked_list.py
+++ other_practice/linked_list.py
@@ -11,11 +11,28 @@ node_1 = Node(1)
 node_12.next = node_92
 node_92.next = node_26
 node_26.next = node_1
-linked_list = [node_12, node_92, node_26, node_1]
 
-def reverse_linked_list(linked_list):
 
-    print('hi!')
+class LinkedList:
+    def __init__(self, head):
+        self.head = head
+
+    def insert_node(self, prior_node, new_node):
+        print('insert node')
+
+    def insert_at_tail(self, new_node):
+        print('insert at tail')
+
+    def insert_at_head(self, new_node):
+        new_node.next = self.head
+        self.head = new_node
+
+ll = LinkedList(node_12)
+print(ll.head.val)
+
+new_node = Node(100)
+ll.insert_at_head(new_node)
+
 
 # Traverse a linked list
 # Remove duplicates from a linked list
